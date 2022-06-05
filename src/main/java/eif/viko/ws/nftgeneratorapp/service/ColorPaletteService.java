50000c8578c191795f6a590934fa42fdf6e539c3
0x30B7 <mdambrauskas98@gmail.com>
2022-06-05 17:00:27 +0300

    Color palette service improvement

--- src/main/java/eif/viko/ws/nftgeneratorapp/service/ColorPaletteService.java
+++ src/main/java/eif/viko/ws/nftgeneratorapp/service/ColorPaletteService.java
@@ -1,43 +1,49 @@
 package eif.viko.ws.nftgeneratorapp.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.stereotype.Service;
 
+import java.awt.*;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.awt.*;
+import java.util.Map;
 
 @Service
 public class ColorPaletteService {
 
     public static final int MAX_COLORS_PER_FETCH = 5;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectReader objectReader = objectMapper.reader();
+    private final ObjectWriter objectWriter = objectMapper.writer();
 
     public List<Color> fetchPalette() {
         try {
             HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create("http://colormind.io/api/"))
-                    .POST(HttpRequest.BodyPublishers.ofString("{\"model\":\"default\"}")).build();
+                    .POST(HttpRequest.BodyPublishers.ofString(
+                            objectWriter.writeValueAsString(Map.of("model", "default"))))
+                    .build();
+
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            String responseString = response.body();
-            responseString = responseString.replace("{\"result\":", "");
-            responseString = responseString.replace("}\n", "");
-            responseString = responseString.replace("[", "");
-            responseString = responseString.replace("]", "");
-            var valueArray = responseString.split(",");
+            Map<String, Object> responseJson = objectReader.readValue(response.body(), Map.class);
+            List<List<Integer>> result = (List<List<Integer>>) responseJson.get("result");
+
             List<Color> colors = new ArrayList<>();
-            for (int i = 0; i < MAX_COLORS_PER_FETCH * 3; i += 3) {
-                Color color = new Color(Integer.parseInt(valueArray[i]), Integer.parseInt(valueArray[i + 1]), Integer.parseInt(valueArray[i + 2]), 128);
-                colors.add(color);
+            for (List<Integer> colorRGB : result) {
+                colors.add(new Color(colorRGB.get(0), colorRGB.get(1), colorRGB.get(2)));
             }
+
             return colors;
         } catch (IOException | InterruptedException e) {
-            System.out.println("Unsuccessful color palette service endpoint call; " + e.getMessage());
+            System.err.println("Unsuccessful color palette service endpoint call: " + e.getMessage());
             return null;
         }
     }
